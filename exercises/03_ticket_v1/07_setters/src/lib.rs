1d1337de8976279d2809a52b89c2c9150cda335c
Walber Moreira <walber3@gmail.com>
2024-06-03 08:40:44 -0300

    07 setters

--- exercises/03_ticket_v1/07_setters/src/lib.rs
+++ exercises/03_ticket_v1/07_setters/src/lib.rs
@@ -11,21 +11,9 @@ pub struct Ticket {
 
 impl Ticket {
     pub fn new(title: String, description: String, status: String) -> Ticket {
-        if title.is_empty() {
-            panic!("Title cannot be empty");
-        }
-        if title.len() > 50 {
-            panic!("Title cannot be longer than 50 bytes");
-        }
-        if description.is_empty() {
-            panic!("Description cannot be empty");
-        }
-        if description.len() > 500 {
-            panic!("Description cannot be longer than 500 bytes");
-        }
-        if status != "To-Do" && status != "In Progress" && status != "Done" {
-            panic!("Only `To-Do`, `In Progress`, and `Done` statuses are allowed");
-        }
+        validate_title(&title);
+        validate_description(&description);
+        validate_status(&status);
 
         Ticket {
             title,
@@ -37,6 +25,18 @@ impl Ticket {
     pub fn title(&self) -> &String {
         &self.title
     }
+    pub fn set_title(&mut self, new_title: String) {
+        validate_title(&new_title);
+        self.title = new_title;
+    }
+    pub fn set_description(&mut self, new_description: String) {
+        validate_description(&new_description);
+        self.description = new_description;
+    }
+    pub fn set_status(&mut self, new_status: String) {
+        validate_status(&new_status);
+        self.status = new_status;
+    }
 
     pub fn description(&self) -> &String {
         &self.description
@@ -44,6 +44,30 @@ impl Ticket {
 
     pub fn status(&self) -> &String {
         &self.status
+    }
+}
+
+fn validate_status(status: &String) {
+    if *status != "To-Do" && *status != "In Progress" && *status != "Done" {
+        panic!("Only `To-Do`, `In Progress`, and `Done` statuses are allowed");
+    }
+}
+
+fn validate_description(description: &String) {
+    if description.is_empty() {
+        panic!("Description cannot be empty");
+    }
+    if description.len() > 500 {
+        panic!("Description cannot be longer than 500 bytes");
+    }
+}
+
+fn validate_title(title: &String) {
+    if title.is_empty() {
+        panic!("Title cannot be empty");
+    }
+    if title.len() > 50 {
+        panic!("Title cannot be longer than 50 bytes");
     }
 }
 
